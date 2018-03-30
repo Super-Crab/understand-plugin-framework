41be4a0b5c4df1ec6e54488fb453c8c56034839f
解佩旋 <pxxie@Ctrip.com>
2018-03-30 11:15:49 +0800

    add onTransact

--- aidl-server/src/main/java/com/supercrab/android/aidl_server/IRemoteService.java
+++ aidl-server/src/main/java/com/supercrab/android/aidl_server/IRemoteService.java
@@ -3,6 +3,7 @@ package com.supercrab.android.aidl_server;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.os.Parcel;
 import android.os.RemoteException;
 import android.util.Log;
 
@@ -16,6 +17,8 @@ import java.util.List;
  */
 
 public class IRemoteService extends Service {
+    private static final String TAG = "IRemoteService";
+    private static final String PACKAGE_SAYHI = "com.supercrab.android.aidl_client";
     private ArrayList<Person> persons;
 
     @Override
@@ -27,7 +30,7 @@ public class IRemoteService extends Service {
     private IBinder mBinder = new IRemoteAidl.Stub() {
         @Override
         public int add(int num1, int num2) throws RemoteException {
-            Log.d("TAG", "服务已开启");
+            Log.d(TAG, "服务已开启" + num1 + "   " + num2);
             return num1 * num2;
         }
 
@@ -35,6 +38,22 @@ public class IRemoteService extends Service {
         public List<Person> addPerson(Person p) throws RemoteException {
             persons.add(p);
             return persons;
+        }
+
+        @Override
+        public boolean onTransact(int code, Parcel data, Parcel reply, int flags) throws RemoteException {
+
+            String packageName = null;
+            String[] packages = IRemoteService.this.getPackageManager().
+                    getPackagesForUid(getCallingUid());
+            if (packages != null && packages.length > 0) {
+                packageName = packages[0];
+            }
+            Log.d(TAG, "onTransact: " + packageName);
+            if (!PACKAGE_SAYHI.equals(packageName)) {
+                return false;
+            }
+            return super.onTransact(code, data, reply, flags);
         }
     };
 }
